c8aca08f4c7b0b7d94fd9212392efa4dd4e4eeb1
tz309806 <codebackup6422@gmail.com>
2017-09-28 12:07:20 -0400

    f

---
+++ src/main/java/com/practice/technolohy/spring/SpringTechLearn/App.java
@@ -0,0 +1,11 @@
+package com.practice.technolohy.spring.SpringTechLearn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Yes");
+	}
+}
